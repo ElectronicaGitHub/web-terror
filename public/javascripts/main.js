89c318a9dcea0d600353aa2cb66056cfe4c77bb1
Philip <molo4nik11@gmail.com>
2014-11-23 02:29:40 +0300

    new favicon

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,16 +4,7 @@ $(document).ready(function() {
 		opacity : 1
 	}, 3000);
 
-	// var dot_size = '2px',
-	// 	animation_time = 1000;
-	// var s = skrollr.init();
-
-	// $('body').on('click', '.dot-middle', function() {
-	// 	$('.second-scene').addClass('rect-transform')
-	// })
-
 	$('body').on('click', '.title', function() {
-		console.log('clicked')
 		$('body').off('click', '.title');
 		$('.error-screen').css({
 			display : 'block'
@@ -21,27 +12,5 @@ $(document).ready(function() {
 		makeRects(200, 300);
 		startGlitching(40, 40);
 		screenOff();
-
-	// 	console.log('transition start');
-	// 	// делаем чтобы было по центру
-	// 	$('.title').animate({
-	// 		'height' : dot_size,
-	// 		'line-height' : dot_size 
-	// 	}, {
-	// 		duration : animation_time,
-	// 		easing : 'easeInOutBack'
-	// 	});
-	// 	// уменьшаем надпись
-	// 	$('.web, .terror').animate({
-	// 		'font-size' : dot_size,
-	// 		'line-height' : dot_size
-	// 	}, {
-	// 		duration : animation_time,
-	// 		easing : 'easeInOutBack'
-	// 	});
-	// 	// выносим сцену на передний план чтоб можно было тыкнуть
-	// 	$('.main-scene').addClass('dot-scene-activate');
-	// 	// увеличиваем точку
-	// 	$('.centrifyer').addClass('dot-big');
 	})
 })
