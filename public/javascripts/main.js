2ad354dc49fde8b056ac1f241f6f3b638863abb2
Philip <molo4nik11@gmail.com>
2014-11-23 02:37:56 +0300

    change auto appear

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
 		opacity : 1
 	}, 3000);
 
-	$('body').on('click', '.title', function() {
+	// $('body').on('click', '.title', function() {
+	setTimeout(function() {
+
+	// })
 		$('body').off('click', '.title');
 		$('.error-screen').css({
 			display : 'block'
@@ -12,5 +15,5 @@ $(document).ready(function() {
 		makeRects(200, 300);
 		startGlitching(40, 40);
 		screenOff();
-	})
+	}, 2500);
 })
