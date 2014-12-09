44d63f4f8f2a7a0605bc104735bca9b7ac489fc3
Philip <molo4nik11@gmail.com>
2014-12-09 19:12:25 +0300

    style modified

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -34,17 +34,17 @@ var works = [
 
 $(document).ready(function() {
 
-	// var lang = window.navigator.language || window.navigator.userLanguage;
+	var lang = window.navigator.language || window.navigator.userLanguage;
 
-	// var rus = lang.match('ru');
-	// if (rus) {
-	// 	$('p.web').text('ВЕБ');
-	// 	$('p.web').attr('data-glitch','ВЕБ');
-	// 	$('p.terror').text('ТЕРРОР');
-	// 	$('p.terror').attr('data-glitch', 'ТЕРРОР');
-	// 	$('.main-view-wtlabel').text('ВЕБ ТЕРРОР');
-	// 	$('.main-view-contacts .phone').text('+7 909 6952723');
-	// }
+	var rus = lang.match('ru');
+	if (rus) {
+		$('p.web').text('ВЕБ');
+		$('p.web').attr('data-glitch','ВЕБ');
+		$('p.terror').text('ТЕРРОР');
+		$('p.terror').attr('data-glitch', 'ТЕРРОР');
+		$('.main-view-wtlabel').text('ВЕБ ТЕРРОР');
+		$('.main-view-contacts .phone').text('+7 909 6952723');
+	}
 
 	$('.title').animate({
 		opacity : 1
